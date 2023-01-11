86a0b87c873d2f47e1541fdc9b52b59ca644f03d
DavidKwon7 <khjunejune10@gmail.com>
2023-01-11 16:29:53 +0900

    [BUILD] buildSrc + kotlinDSL 적용

---
+++ app/build.gradle.kts
@@ -0,0 +1,102 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(Dependency.Hilt.HILT_PLUGIN)
+    }
+}
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    namespace = "com.flow.moviesearchapp"
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.flow.moviesearchapp"
+        minSdk = 24
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    dataBinding {
+        enable = true
+    }
+}
+
+dependencies {
+
+    implementation(Dependency.KTX.CORE)
+    implementation(Dependency.AndroidX.APP_COMPAT)
+    implementation(Dependency.AndroidX.MATERIAL)
+    implementation(Dependency.AndroidX.CONSTRAINT_LAYOUT)
+    implementation(Dependency.Nav.NAV_FRAGMENT)
+    implementation(Dependency.Nav.NAV_UI)
+    testImplementation(Dependency.Test.JUNIT)
+    androidTestImplementation(Dependency.AndroidTest.TEST_RUNNER)
+    androidTestImplementation(Dependency.AndroidTest.ESPRESSO_CORE)
+
+    implementation(Dependency.Hilt.HILT)
+    kapt(Dependency.Hilt.HILT_KAPT)
+
+    implementation(Dependency.Log.TIMBER)
+
+    testImplementation(Dependency.Test.TRUTH)
+    testImplementation(Dependency.Test.MOCKITO)
+    testImplementation(Dependency.Test.CORE_TEST)
+    testImplementation(Dependency.Test.MOCKK)
+
+    implementation(Dependency.Remote.RETROFIT)
+    implementation(Dependency.Remote.CONVERTER)
+    implementation(Dependency.Remote.HTTP)
+
+    implementation(Dependency.Coroutine.COROUTINE_CORE)
+    implementation(Dependency.Coroutine.ANDROID)
+    testImplementation(Dependency.Coroutine.TEST)
+
+    implementation(Dependency.Room.RUNTIME)
+    kapt(Dependency.Room.COMPILER)
+    implementation(Dependency.Room.ROOM_KTX)
+
+    implementation(Dependency.LifeCycle.VM)
+    implementation(Dependency.LifeCycle.EXTENSIONS)
+    implementation(Dependency.LifeCycle.LIVEDATA)
+
+    implementation(Dependency.Paging.PAGING)
+
+    implementation(Dependency.Glide.GLIDE)
+    implementation(Dependency.Glide.GLIDE_COMPILER)
+
+    implementation(Dependency.Nav.NAV_UI)
+    implementation(Dependency.Nav.NAV_FRAGMENT)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "MovieSearchApp"
-include ':app'
+include (":app")
