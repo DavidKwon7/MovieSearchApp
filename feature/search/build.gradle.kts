b5873be43f2cb7345035bd6c9fdad3686adf78e3
DavidKwon7 <khjunejune10@gmail.com>
2023-01-13 02:43:32 +0900

    [FEAT] SearchViewModel 추가

--- feature/search/build.gradle.kts
+++ feature/search/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -35,10 +38,29 @@ android {
 
 dependencies {
 
+    implementation(project(":core:common"))
+    implementation(project(":core:data"))
+    implementation(project(":core:domain"))
+
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(Dependency.Hilt.HILT)
+    kapt(Dependency.Hilt.HILT_KAPT)
+
+    implementation(Dependency.LifeCycle.VM)
+    implementation(Dependency.LifeCycle.EXTENSIONS)
+    implementation(Dependency.LifeCycle.LIVEDATA)
+
+    implementation(Dependency.Paging.PAGING)
+
+    implementation(Dependency.Glide.GLIDE)
+    implementation(Dependency.Glide.GLIDE_COMPILER)
+
+    implementation(Dependency.Nav.NAV_UI)
+    implementation(Dependency.Nav.NAV_FRAGMENT)
 }
